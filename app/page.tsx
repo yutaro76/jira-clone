f1646cb9fb86466d9b3a1d3be9f846a4eaf76223
yutaro okuda <yutaro.okuda.76@gmail.com>
2025-01-07 08:00:41 +0800

    [Resolving lint errors] エラーの修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { SelectTrigger } from '@/components/ui/select';
-import { TestComponent } from '@/features/test';
 
 export default function Home() {
   return (
