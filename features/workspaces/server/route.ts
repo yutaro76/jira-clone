bbe237b5ccce1e0bfa517a59be04d9c53739680c
yutaro okuda <yutaro.okuda.76@gmail.com>
2025-01-09 17:02:35 +0800

    [Creating workspace members] 自分が作ったワークスペースのみ表示されるようにする

--- features/workspaces/server/route.ts
+++ features/workspaces/server/route.ts
@@ -2,16 +2,35 @@ import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { createWorkspaceSchema } from '../schema';
 import { sessionMiddleware } from '@/lib/session-middleware';
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from '@/config';
-import { ID } from 'node-appwrite';
+import {
+  DATABASE_ID,
+  IMAGES_BUCKET_ID,
+  MEMBERS_ID,
+  WORKSPACES_ID,
+} from '@/config';
+import { ID, Query } from 'node-appwrite';
+import { MemberRole } from '@/features/members/types';
+import { genereateInviteCode } from '@/lib/utils';
 
 const app = new Hono()
   .get('/', sessionMiddleware, async (c) => {
+    const user = c.get('user');
     const databases = c.get('databases');
+
+    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+      Query.equal('userId', user.$id),
+    ]);
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
 
     const workspaces = await databases.listDocuments(
       DATABASE_ID,
-      WORKSPACES_ID
+      WORKSPACES_ID,
+      [Query.orderDesc('$createdAt'), Query.contains('$id', workspaceIds)]
     );
 
     return c.json({ data: workspaces });
@@ -55,8 +74,16 @@ const app = new Hono()
           name,
           userId: user.$id,
           imageUrl: uploadedImageUrl,
+          inviteCode: genereateInviteCode(6),
         }
       );
+
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: MemberRole.ADMIN,
+      });
+
       return c.json({ data: workspace });
     }
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function genereateInviteCode(length: number) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
